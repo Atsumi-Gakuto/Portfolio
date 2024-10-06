bc0d443481fc6c21b6d6b2e4f646c464fdc3046e
Atsumi-Gakuto <b24702bb@aitech.ac.jp>
2024-10-06 23:59:51 +0900

    最初の記事を作成

--- src/ts/popup_manager.ts
+++ src/ts/popup_manager.ts
@@ -35,6 +35,11 @@ class PopupManager {
     private isTransitioning: boolean = false;
 
     /**
+     * 記事のHTMLファイルの高さを監視し、iframeの高さを調整するオブサーバ。
+     */
+    private observer?: ResizeObserver;
+
+    /**
      * コンストラクタ
      * @param parent 親のメインクラスへの参照子
      */
@@ -48,9 +53,22 @@ class PopupManager {
      */
     public showArticle(articleData: PopupArticleData): void {
         if(!this.isTransitioning) {
-            (document.getElementById("article_image") as HTMLImageElement).src = articleData.thumbnail;
-            (document.getElementById("article_title") as HTMLHeadingElement).innerText = articleData.title;
-            (document.getElementById("article_body_html") as HTMLIFrameElement).src = articleData.body;
+            const articleBodyHtml: HTMLIFrameElement = document.getElementById("article_body_html") as HTMLIFrameElement;
+            const loadingArea: HTMLDivElement = document.getElementById("article_loading") as HTMLDivElement;
+            const fileNameMatcher: RegExp = /[^\/]+\.\w+$/;
+            if(articleBodyHtml.src.length == 0 || articleBodyHtml.src.match(fileNameMatcher)![0] != articleData.body.match(fileNameMatcher)![0]) {
+                articleBodyHtml.classList.add("hidden");
+                loadingArea.classList.remove("hidden");
+                (document.getElementById("article_image") as HTMLImageElement).src = articleData.thumbnail;
+                (document.getElementById("article_title") as HTMLHeadingElement).innerText = articleData.title;
+                articleBodyHtml.src = articleData.body;
+            }
+            this.observer = new ResizeObserver(() => articleBodyHtml.style.height = `${articleBodyHtml.contentWindow!.document.body.scrollHeight + window.innerWidth - document.body.clientWidth + 25}px`);
+            articleBodyHtml.addEventListener("load", () => {
+                this.observer!.observe(articleBodyHtml);
+                articleBodyHtml.classList.remove("hidden");
+                loadingArea.classList.add("hidden");
+            }, { once: true });
             const tagArea: HTMLDivElement = document.getElementById("article_tags") as HTMLDivElement;
             while(tagArea.children.length > 0) tagArea.children.item(0)!.remove();
             articleData.tagNames.forEach((tagName: string) => this.parent.tagManager.insertTagElement(tagArea, tagName, false));
@@ -75,6 +93,7 @@ class PopupManager {
             (document.getElementById("article_area") as HTMLDivElement).classList.remove("popup_transition");
             popupBackground.addEventListener("transitionend", () => {
                 (document.getElementById("popup_area") as HTMLDivElement).classList.add("hidden");
+                this.observer?.disconnect();
                 this.isTransitioning = false;
             }, { once: true });
             this.isTransitioning = true;
