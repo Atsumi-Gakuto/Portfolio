70fa53146da67318799b9d9c0455a48f5d9a8bbc
Atsumi-Gakuto <b24702bb@aitech.ac.jp>
2024-10-06 22:11:01 +0900

    閉じるバツを廃止し、背景のクリックで閉じれるように変更

--- src/ts/popup_manager.ts
+++ src/ts/popup_manager.ts
@@ -30,6 +30,11 @@ class PopupManager {
     private readonly parent: Main;
 
     /**
+     * 移行アニメーションの再生中かどうか
+     */
+    private isTransitioning: boolean = false;
+
+    /**
      * コンストラクタ
      * @param parent 親のメインクラスへの参照子
      */
@@ -42,36 +47,45 @@ class PopupManager {
      * @param articleData 表示する記事のデータ
      */
     public showArticle(articleData: PopupArticleData): void {
-        (document.getElementById("article_image") as HTMLImageElement).src = articleData.thumbnail;
-        (document.getElementById("article_title") as HTMLHeadingElement).innerText = articleData.title;
-        (document.getElementById("article_body_html") as HTMLIFrameElement).src = articleData.body;
-        const tagArea: HTMLDivElement = document.getElementById("article_tags") as HTMLDivElement;
-        while(tagArea.children.length > 0) tagArea.children.item(0)!.remove();
-        articleData.tagNames.forEach((tagName: string) => this.parent.tagManager.insertTagElement(tagArea, tagName, false));
-        (document.getElementById("popup_area") as HTMLDivElement).classList.remove("hidden");
-        setTimeout(() => {
-            (document.getElementById("popup_background") as HTMLDivElement).classList.add("popup_transition");
-            (document.getElementById("popup_content") as HTMLDivElement).classList.add("popup_transition");
-        }, 1);
+        if(!this.isTransitioning) {
+            (document.getElementById("article_image") as HTMLImageElement).src = articleData.thumbnail;
+            (document.getElementById("article_title") as HTMLHeadingElement).innerText = articleData.title;
+            (document.getElementById("article_body_html") as HTMLIFrameElement).src = articleData.body;
+            const tagArea: HTMLDivElement = document.getElementById("article_tags") as HTMLDivElement;
+            while(tagArea.children.length > 0) tagArea.children.item(0)!.remove();
+            articleData.tagNames.forEach((tagName: string) => this.parent.tagManager.insertTagElement(tagArea, tagName, false));
+            (document.getElementById("popup_area") as HTMLDivElement).classList.remove("hidden");
+            setTimeout(() => {
+                const popupBackground:  HTMLDivElement = document.getElementById("popup_background") as HTMLDivElement;
+                popupBackground.classList.add("popup_transition");
+                popupBackground.addEventListener("transitionend", () => this.isTransitioning = false, { once: true });
+                (document.getElementById("article_area") as HTMLDivElement).classList.add("popup_transition");
+                this.isTransitioning = true;
+            }, 1);
+        }
     }
 
     /**
      * 記事のポップアップを閉じる。
      */
     public closeArticle(): void {
-        const popupBackground:  HTMLDivElement = document.getElementById("popup_background") as HTMLDivElement;
-        popupBackground.classList.remove("popup_transition");
-        (document.getElementById("popup_content") as HTMLDivElement).classList.remove("popup_transition");
-        popupBackground.addEventListener("transitionend", () => (document.getElementById("popup_area") as HTMLDivElement).classList.add("hidden"), {
-            once: true
-        });
+        if(!this.isTransitioning) {
+            const popupBackground:  HTMLDivElement = document.getElementById("popup_background") as HTMLDivElement;
+            popupBackground.classList.remove("popup_transition");
+            (document.getElementById("article_area") as HTMLDivElement).classList.remove("popup_transition");
+            popupBackground.addEventListener("transitionend", () => {
+                (document.getElementById("popup_area") as HTMLDivElement).classList.add("hidden");
+                this.isTransitioning = false;
+            }, { once: true });
+            this.isTransitioning = true;
+        }
     }
 
     /**
      * 初期化関数
      */
     public init(): void {
-        (document.getElementById("popup_close_button") as HTMLInputElement).addEventListener("click", () => this.closeArticle());
+        (document.getElementById("popup_background") as HTMLDivElement).addEventListener("click", () => this.closeArticle());
         (document.getElementById("article_close_button") as HTMLButtonElement).addEventListener("click", () => this.closeArticle());
     }
 }
